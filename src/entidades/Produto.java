903c8282375b6c5fd79db44f0f1dd030cab2b9c7
Gabriel Santana <gabrielsdeo2009@gmail.com>
2025-06-06 12:15:52 -0400

    Produto 0606

---
+++ src/entidades/Produto.java
@@ -0,0 +1,74 @@
+package entidades;
+
+import javax.swing.*;
+
+public class Produto {
+
+    private String nome;
+    private double preco;
+    private int quant;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuant() {
+        return quant;
+    }
+
+    public void setQuant(int quant) {
+        this.quant = quant;
+    }
+
+    public Produto(String nome, double preco, int quant){
+
+        this.nome = nome;
+        this.preco = preco;
+        this.quant = quant;
+    }
+
+    public double valorTotalEstoque() {
+
+        double valorTotal = getPreco()*getQuant();
+        return valorTotal;
+
+    }
+
+    public void adicionarProdutos(int quantMais) {
+
+        setQuant(getQuant() + quantMais);
+        System.out.println(toString());
+    }
+
+    public void removerProdutos(int quantMenos) {
+
+        setQuant(getQuant() - quantMenos);
+        System.out.println(toString());
+    }
+
+    public String toString(){
+        return "Nome:"
+        +getNome()
+        +" , R$"
+        + String.format("%.2f" , getPreco())
+        +getQuant()
+        +"unidades, Valor Total: R$"
+        + String.format("%.2f", valorTotalEstoque());
+
+
+
+
+    }
+}
